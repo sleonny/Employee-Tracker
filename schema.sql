829135c335fc1f533a0bbbf35b2e2bfe478dbe07
Sean L <leonardmsean@gmail.com>
2023-05-11 15:25:07 -0400

    finished schema hopefully

--- schema.sql
+++ schema.sql
@@ -16,3 +16,15 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERNCES department(id),
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERNCES role(id),
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERNCES employee(id)
+    ON DELETE SET NULL
+);
