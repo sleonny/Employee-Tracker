afe9c21434167fd72eb26ec94123035b982f07a4
Sean L <leonardmsean@gmail.com>
2023-05-11 15:48:59 -0400

    added employee table seed

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE role (
     ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
--- seeds.sql
+++ seeds.sql
@@ -13,3 +13,13 @@ VALUES  ('Sales Lead', 100000, 1),
         ('Accountant', 125000, 3),
         ('Legal Team Lead', 250000, 4),
         ('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL),
+        ('Mike', 'Chan', 2, 1),
+        ('Ashley', 'Rodriguez', 3, NULL),
+        ('Kevin', 'Tupik', 4, 3), 
+        ('Kunal', 'Singh', 5, NULL),
+        ('Malia', 'Brown', 6, 5),
+        ('Sarah', 'Leonard', 7, NULL),
+        ('Tom', 'Allen', 8, 7);
