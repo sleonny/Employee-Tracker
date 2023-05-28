bc71201e7157664c7349ddcaf501eec6d50928df
Sean L <leonardmsean@gmail.com>
2023-05-28 11:15:11 -0400

    added foreign keys to the tables

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,21 @@ USE employees_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+    );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     dept_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
