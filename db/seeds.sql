d47598850f6b4def6aae43445c86c8a0f3b38361
Sean L <leonardmsean@gmail.com>
2023-06-02 08:51:26 -0400

    added seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,28 @@
--- departments.seeds.sql
+-- Seed data for department table
+INSERT INTO department (department_name) VALUES
+    ('Coaches'),
+    ('Forwards'),
+    ('Midfielders'),
+    ('Defenders');
 
--- Inserting data into the department table
-INSERT INTO department (department_name) VALUES ('Finance');
-INSERT INTO department (department_name) VALUES ('Human Resources');
-INSERT INTO department (department_name) VALUES ('Marketing');
-INSERT INTO department (department_name) VALUES ('Sales');
-INSERT INTO department (department_name) VALUES ('Engineering');
-INSERT INTO department (department_name) VALUES ('Customer Service');
-INSERT INTO department (department_name) VALUES ('IT');
-INSERT INTO department (department_name) VALUES ('Operations');
+-- Seed data for role table
+INSERT INTO role (title, salary, dept_id) VALUES
+    ('Manager', 600000, 1),
+    ('Head Physio', 400000, 1),
+    ('Striker', 700000, 2),
+    ('Winger', 500000, 2),
+    ('Offensive Midfielder', 550000, 3),
+    ('Defensive Midfielder', 350000, 3),
+    ('Backs', 65000, 4),
+    ('Goalies', 45000, 4);
 
--- roles.seeds.sql
-
--- Inserting data into the role table
-INSERT INTO role (title, salary, dept_id) VALUES ('Accountant', 50000, 1);
-INSERT INTO role (title, salary, dept_id) VALUES ('HR Manager', 60000, 2);
-INSERT INTO role (title, salary, dept_id) VALUES ('Marketing Specialist', 45000, 3);
-INSERT INTO role (title, salary, dept_id) VALUES ('Sales Representative', 55000, 4);
-INSERT INTO role (title, salary, dept_id) VALUES ('Software Engineer', 80000, 5);
-INSERT INTO role (title, salary, dept_id) VALUES ('Customer Service Representative', 40000, 6);
-INSERT INTO role (title, salary, dept_id) VALUES ('IT Manager', 70000, 7);
-INSERT INTO role (title, salary, dept_id) VALUES ('Operations Supervisor', 55000, 8);
-
--- employees.seeds.sql
-
--- Inserting data into the employees table
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, 1);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Jane', 'Smith', 2, 2);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Johnson', 3, 3);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Emily', 'Davis', 4, 4);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('David', 'Lee', 5, 5);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Wilson', 6, 6);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Matthew', 'Clark', 7, 7);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Jennifer', 'Taylor', 8, 8);
+-- Seed data for employees table
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+    ('Carlo', 'Ancelloti', 1, 0),
+    ('Karim', 'Benzema', 2, 1),
+    ('Vinicius', 'Junior', 3, 1),
+    ('Tony', 'Kroos', 4, 2),
+    ('Luka', 'Modric', 5, 3),
+    ('Danny', 'Carvajal', 6, 3),
+    ('Antonio', 'Rudiger', 7, 4),
+    ('Thibaut', 'Courtois', 8, 7);
