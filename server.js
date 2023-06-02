f53850db8318f78125a20abdfa0a1b618a3149bb
Sean L <leonardmsean@gmail.com>
2023-06-02 09:13:34 -0400

    started main menu prompts

--- server.js
+++ server.js
@@ -12,4 +12,48 @@ var connection = mysql.createConnection({
 console.log('Connected to the Employee Database')
 );
 
-
+const mainMenu = () => {
+    return inquirer.prompt([{
+        name: 'Main_Menu',
+        type: 'list',
+        message: 'Where would you like to start?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    }])
+    .then(userChoice => {
+        switch (userChoice.mainMenu) {
+            case 'View all departments':
+                function();
+                break;
+            case 'View all roles':
+                function();
+                break;
+            case 'View all employees':
+                function();
+                break;
+            case 'Add a department':
+                function();
+                break;
+            case 'Add a role':
+                function();
+                break;
+            case 'Add an employee':
+                function();
+                break;
+            case 'Update an employee role':
+                function();
+                break;
+            default:
+                process.exit();
+                
+        }
+    });
+};
